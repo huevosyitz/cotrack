7b89bdd26d24caf9403d068e4c4326fb8c1d4a6a
huevos_yitz <huevos_yitz@outlook.com>
2025-01-19 23:57:49 +0800

    init transactions data

--- lib/config/data_init.dart
+++ lib/config/data_init.dart
@@ -1,8 +1,15 @@
+import 'package:cached_query_flutter/cached_query_flutter.dart';
 import 'package:cotrack/core/services/services.dart';
 import 'package:watch_it/watch_it.dart';
 
 class DataInit {
   static Future<void> init() async {
     await di<TransactionCategoryService>().setupTransactionCategories();
+
+    final transactions =
+        await di.get<TransactionService>().getAllMyTransactions();
+
+    CachedQuery.instance.updateQuery(
+        key: TransactionService.queryKey, updateFn: (_) => transactions);
   }
 }
